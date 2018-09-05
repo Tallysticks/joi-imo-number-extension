a51a7f4de04f508dcb6c9ddf1b041496519ce943
George Wilson <george@g-wilson.co.uk>
2018-09-05 18:34:02 +0100

    Update error types to avoid collisions with other Joi extensions

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ module.exports = joi => {
     base: joi.string(),
     name: 'string',
     language: {
-      missingPrefix: 'needs to have a valid IMO number prefix',
-      invalidFormat: 'needs to be a valid vessel IMO number',
+      ImoMissingPrefix: 'needs to have a valid IMO number prefix',
+      ImoInvalidFormat: 'needs to be a valid vessel IMO number',
     },
     pre(value, state, options) {
       return value
@@ -24,9 +24,9 @@ module.exports = joi => {
             return value
           }
           if (imonumber.validate(`IMO ${value}`)) {
-            return this.createError('string.missingPrefix', { value }, state, options)
+            return this.createError('string.ImoMissingPrefix', { value }, state, options)
           }
-          return this.createError('string.invalidFormat', { value }, state, options)
+          return this.createError('string.ImoInvalidFormat', { value }, state, options)
         },
       },
     ],
